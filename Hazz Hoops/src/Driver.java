2a6e844b29fad9e4e601b2cf352782aece656920
1851629 <1851629@01-19753.pusd.dom>
2023-05-09 13:16:48 -0700

    Driver

---
+++ Hazz Hoops/src/Driver.java
@@ -0,0 +1,122 @@
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.GridLayout;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+
+import javax.swing.JLabel;
+public class Driver extends JPanel implements ActionListener, MouseListener, KeyListener  {
+
+	
+	public Driver() {
+		
+		JFrame f = new JFrame("IDK Yet");
+		f.setSize(new Dimension(1200, 900));
+		f.setBackground(new Color(0, 0, 0));
+		f.add(this);
+		f.setResizable(false);
+		f.setLayout(new GridLayout(1, 2));
+		f.addMouseListener(this);
+		f.addKeyListener(this);
+		Timer t = new Timer(16, this);
+		t.start();
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setVisible(true);
+		/*BufferedImage cursorImg;
+		try {
+					cursorImg =  ImageIO.read(new File("crosshair img.png"));
+					Cursor blankCursor = Toolkit.getDefaultToolkit().createCustomCursor(
+						    cursorImg, new Point(0, 0), "blank cursor");
+					f.getContentPane().setCursor(blankCursor);
+
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}	*/
+		
+
+		
+		}
+	public static void main(String[] arg) {
+		Driver f = new Driver();
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+	
+}
