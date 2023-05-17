9a5adac6523765da46544166f1b6867c9810ae7e
1861402 <1861402@01-19961.pusd.dom>
2023-05-17 11:28:58 -0700

    Added menu screen

---
+++ Hazz Hoops/src/MenuScreen.java
@@ -0,0 +1,39 @@
+import java.awt.Rectangle;
+import java.awt.event.MouseEvent;
+
+public class MenuScreen extends Background {
+	
+	public MenuScreen(){
+		super("HomeScreen.png");
+	}
+	
+	
+	
+	
+	//selecting buttons
+	
+	
+	public boolean highscore(MouseEvent mouse) {
+		//mouse cursor hit box
+		Rectangle m = new Rectangle(mouse.getX(), mouse.getY(), 30, 30);
+		
+		//button
+		Rectangle s1 = new Rectangle(450, 860, 899, 105);
+		if(m.intersects(s1)) {
+			return true;
+		}
+		return false;
+		
+	}
+	
+	public boolean play(MouseEvent mouse) {
+		
+		//checks if cursor is within the start button
+		if((Math.pow(((mouse.getX()-890)), 2) + Math.pow((mouse.getY() - 450), 2)) <= Math.pow(300, 2)) {
+			return true;
+		}		
+		return false;
+	}
+	
+}
+	
